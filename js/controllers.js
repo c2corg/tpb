3ee68c6eea93f4d4b53b120a58a38d3d79c764ad
denis.leemann <leemannd@gmail.com>
2014-06-30 10:04:46 +0200

    MAJ Submodule et template OnsenUI

---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+myApp.controller('AboutCtrl', function ($scope){
+    
+});
+
+myApp.controller('showMenuStat', function ($scope){
+    $scope.goRoute=route;
+});
+
+myApp.controller('mainController', function($scope) {
+
+  $scope.goCats = false;
+  });
