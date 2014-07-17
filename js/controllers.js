9814d58f6ac5151d62f5bf191c951495bb983722
denis.leemann <leemannd@gmail.com>
2014-07-17 12:57:14 +0200

    Travail sur le login

--- js/controllers.js
+++ js/controllers.js
@@ -1,32 +1,4 @@
 'use strict';
-//
-//myApp.controller('AboutCtrl', function ($scope){
-//    
-//});
-//
-//myApp.controller('showMenuStat', function ($scope){
-//    $scope.goRoute=route;
-//});
-//
-//myApp.controller('mainController', function($scope) {
-//
-//  $scope.goCats = false;
-//  });
-//  
-//myApp.controller('settings', function ($scope){
-//    
-//});
-//
-//myApp.controller('sortie', function($scope){
-//    
-//});
-//    
-//myApp.controller('MorningCtrl', function($scope){
-//   $scope.greeting = "Good Morning!"; 
-//});
-
-
-
 //var controllers = {};
 //
 //controllers.SearchList = function ($scope){
@@ -50,35 +22,33 @@
 /**
  * Permet le controle du timer
  */
-
-
 myApp.controller('timerCtrl', function MyCtrl($scope, chronoService, $rootScope) {
 
     $scope.showStart = $rootScope.showStart;
     $scope.showEnregistrer = $rootScope.showEnregistrer;
     $scope.pauseTime = null;
-    
+
     $scope.start = function() {
-        if(null != $rootScope.pauseTimer){
+        if (null != $rootScope.pauseTimer) {
             $scope.pauseTime = $rootScope.pauseTimer;
         }
-        if(null != $rootScope.startTimer){
+        if (null != $rootScope.startTimer) {
             $scope.time = $rootScope.startTimer;
         }
-        
+
         $scope.showEnregistrer = true;
         $scope.showStart = false;
-        
+
         // Update du status dans le rootscope
-        $rootScope.showEnregistrer =$scope.showEnregistrer;
+        $rootScope.showEnregistrer = $scope.showEnregistrer;
         $rootScope.showStart = $scope.showStart;
-        
+
         if (null != $scope.time) {
             $scope.time = $scope.time + (Date.now() - $scope.pauseTime);
         } else {
             $scope.time = Date.now();
         }
-        
+
         chronoService.start();
         $rootScope.startTimer = $scope.time;
     };
@@ -87,7 +57,7 @@ myApp.controller('timerCtrl', function MyCtrl($scope, chronoService, $rootScope)
         $rootScope.showStart = $scope.showStart;
         $scope.pauseTime = Date.now();
         chronoService.stop();
-        
+
         $rootScope.pauseTimer = $scope.pauseTime;
     };
     chronoService.addTimer('myTimer', {interval: 500});
@@ -95,46 +65,143 @@ myApp.controller('timerCtrl', function MyCtrl($scope, chronoService, $rootScope)
 
 
 /*****    SETTINGS    *****/
-
-myApp.controller('settingsCtrl', function settingsCtrl($scope){
+myApp.controller('settingsCtrl', function settingsCtrl($scope, $http) {
+    $scope.data = "dataSet";
+    $scope.status = "statusSet";
     $scope.settings = {
         freqGPS: 10,
         GPSDistance: 10,
         battery: 10,
         username: 'illianor',
         password: 'toto'
-    }
+    };
+    
+    $scope.debug = "debugButton";
+    
+    $http.defaults.useXDomain = true;
+    
+    $scope.onLogin = function(){
+        
+        $scope.debug = "functionLogin";
+        $http({
+            method: 'POST',
+            url: 'http://camptocamp.org/login',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            ,
+            data: {
+                login_name: 'illianor',
+                password: 'aaIWFCxG'
+            }
+        }).success(function(data, status, headers, config, scope){
+            $scope.debug= "success";
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+        }).error(function(data, status, headers, config, scope){
+            $scope.debug="error";
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+        });
+    };
 });
 
 
 /*****     GEOLOCATION     *****/
-myApp.controller('gelocation', function($scope, $cordovaGeolocation) {
-  $cordovaGeolocation.getCurrentPosition().then(function(position) {
-      // Position here: position.coords.latitude, position.coords.longitude
+myApp.controller('GeolocationCtrl', function($scope, $cordovaGeolocation) {
+    // Trace vide
+    $scope.trace = [
+    ];
+    // Initialisation d'un point
+    $scope.point = {
+        latitude: 0.000,
+        longitude: 0.000,
+        altitude: 0
+    };
+    //Ajout d'un point à la trace
+    $scope.addPoint = function(point) {
+        $scope.trace.push(point);
+    };
+    
+    // Pas utilisé pour le moment
+    $cordovaGeolocation.getCurrentPosition().then(function(position) {
+        // Position here: position.coords.latitude, position.coords.longitude
     }, function(err) {
-      // error
+        // error
     });
 
-  $cordovaGeolocation.watchPosition().then(function() {
-      // Not currently used
+    /**
+     * Permet de suivre d'avoir la géolocalisation de l'utilisateur et de créer
+     * une trace GPS
+     */
+    $cordovaGeolocation.watchPosition({maximumAge: 300, timeout: 5000, enableHighAccuracy: true}).then(function() {
+
     }, function(err) {
-      // An error occured. Show a message to the user
+        $console.log('error');
     }, function(position) {
-      $scope.latitude = position.coords.latitude;
-      $scope.longitude = position.coords.longitude;
-  });
-  
+        $scope.lat = position.coords.latitude;
+        $scope.lng = position.coords.longitude;
+        $scope.alt = position.coords.altitude;
+        $scope.all = position;
+        $scope.point = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            altitude: position.coords.altitude
+        };
+        $scope.addPoint($scope.point);
+        $scope.$apply();
+    });
+
+    /**
+     * Retour différencié si 
+     * @returns {String}
+     */
     $scope.getLatLng = function() {
-        if(!$scope.lat && !$scope.lng) { return '-45.787, -89.052'; }
-        return $scope.lat.toFixed(3) + ', ' + $scope.lng.toFixed(3);
-    }
-    
+        if (!$scope.lat && !$scope.lng) {
+            return 'No Position';
+        }
+        return $scope.lat.toFixed(3) + ', ' + $scope.lng.toFixed(3) + ', ' + $scope.alt;
+    };
+
     $scope.toggleTrack = function() {
         $cordovaGeolocation.watchPosition().then(function(resp) {
         }, function(err) {
         }, function(position) {
-            $scope.latitude = position.coords.latitude;
-            $scope.longitude = position.coords.longitude;
+            $scope.lat = position.coords.latitude;
+            $scope.lng = position.coords.longitude;
+            $scope.alt = position.coords.altitude;
+            $scope.timestamp = position.timestamp;
+        });
+    };
+//    $scope.addPoint = function($scope){
+//        return $scope.trace.push($scope.getLatLng());
+//    };
+    
+
+});
+
+/**
+ * Controlleur pour le login
+ */
+myApp.controller('loginCtrl', function loginCtrl($scope) {
+    $controller('settingsCtrl', {$scope: $scope});
+    $scope.debug = "bouton";
+    $scope.onLogin = function(user){
+        $http({
+            method: 'POST',
+            url: 'http://camptocamp.org/login',
+            header:{
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: {
+                login_name: 'illianor',
+                password: 'aaIWFCxG'
+            }
+        }).success(function(data, status, headers, config){
+            $scope.data = data;
+            $scope.status = status;
+        }).error(function(data, status, headers, config){
+            $scope.status = status;
         });
     };
 });
--- js/services.js
+++ js/services.js
@@ -1,40 +1,42 @@
-myApp.factory('User', function($http, $q){
-    var factory = {
-	user : -1,
-        
-	// On vérifie si le couple utilisateur + mot de passe est correct
-	login : function(userId, pass){
-	    var deferred = $q.defer();
-	    $http.get(bootstrap + "?controller=Users&action=validerUser&userLogin="+userId+"&mdp="+pass)
-            
-	    // Si le couple est correct, on renvoie le user entier
-	    .success(function(data, status){
-		factory.user = data;
-		console.log("Data : " + data);
-		console.log("Status : " + status);
-		console.log("Authentifié en tant que : " + data);
-		deferred.resolve(factory.user);
-	    })
-                    
-	    // Sinon on renvoie un message d'erreur
-	    .error(function(){
-		console.log("Echec d'authentification");
-		deferred.reject("Factory Users : Erreur lors du login");
-	    });
-	    return deferred.promise;
-	}
-        
-    /*validerUser : function(){
-	    var bar = {};
-	    angular.forEach(factory.bars, function(value, key){
-		if(value.id == key){
-		    bar = value;
-		}
-	    });
-	    return bar;
-	}*/
-        
-        
-    };
-    return factory;
-});
+////myApp.factory('User', function($http, $q){
+////    var factory = {
+////	user : -1,
+////        
+////	// On vérifie si le couple utilisateur + mot de passe est correct
+////	login : function(userId, pass){
+////	    var deferred = $q.defer();
+////	    $http.get(bootstrap + "?controller=Users&action=validerUser&userLogin="+userId+"&mdp="+pass)
+////            
+////	    // Si le couple est correct, on renvoie le user entier
+////	    .success(function(data, status){
+////		factory.user = data;
+////		console.log("Data : " + data);
+////		console.log("Status : " + status);
+////		console.log("Authentifié en tant que : " + data);
+////		deferred.resolve(factory.user);
+////	    })
+////                    
+////	    // Sinon on renvoie un message d'erreur
+////	    .error(function(){
+////		console.log("Echec d'authentification");
+////		deferred.reject("Factory Users : Erreur lors du login");
+////	    });
+////	    return deferred.promise;
+////	}
+////        
+////        
+//        
+//    /*validerUser : function(){
+//	    var bar = {};
+//	    angular.forEach(factory.bars, function(value, key){
+//		if(value.id == key){
+//		    bar = value;
+//		}
+//	    });
+//	    return bar;
+//	}*/
+////        
+////        
+////    };
+////    return factory;
+//});
